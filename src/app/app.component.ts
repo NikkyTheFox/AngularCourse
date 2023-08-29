33417616a0256d7c11e9c70a3610d5a3b5b8753e
NikkyTheFox <adi.pochec@gmail.com>
2023-08-29 15:11:57 +0200

    First Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'] //external .css file
+  styleUrls: ['./app.component.css'] //external .css file
   // As with templates, there is a possiblity to write inline css code. Do not forget about ``
-  styles: [`
+  /*styles: [`
     h3{
       color: red;
     }
-  `]
+  `]*/
 })
 export class AppComponent {
   name = 'Adam';
