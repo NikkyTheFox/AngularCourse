3bc852353adb20a813a335eb9a4b1e532c4ce565
NikkyTheFox <adi.pochec@gmail.com>
2023-08-29 14:44:10 +0200

    TemplateUrl vs Template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //Allows to write inline html code, does not support indents nor new lines
+  template: '<app-server></app-server><app-server></app-server>', 
+  //templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
