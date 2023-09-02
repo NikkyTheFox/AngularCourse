59747b42bbbf4a60778abe0015cfb19be1c49829
NikkyTheFox <adi.pochec@gmail.com>
2023-09-02 13:25:22 +0200

    Input binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{
+    type: 'server',
+    name: 'Testserver',
+    content: 'Just a test'
+  }];
 
 }
